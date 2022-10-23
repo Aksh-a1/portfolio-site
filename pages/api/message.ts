1ba1655d503a97a8cf21e520879114bc471e55e6
Aakash Maurya <akshmourya@gmail.com>
2022-10-23 20:30:10 +0530

    messages logged

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { sendEmail } from '../../utils/emailApiService'
 import maxApiLimitReach from '../../utils/maxApiLimitReach.middleware'
 
 type Data = { msg: string }
@@ -8,7 +7,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { name, email, message, id } = req.body
   maxApiLimitReach(id, res)
   try {
-    console.log(sendEmail({ name, email, message }))
+    console.log(`message: ${JSON.stringify({ name, email, message })}\n`)
     res.status(200).json({ msg: 'Email sent' })
   } catch (e) {
     res.status(500).json({ msg: `Server Error: ${e}` })
