658c247afb97e7f8109638b55caf332bbae00f3a
Aakash Maurya <akshmourya@gmail.com>
2023-11-05 18:55:55 +0530

    about updated

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -12,10 +12,10 @@ const textProps: TextProps = {
     md: '2rem',
     lg: '3rem',
     xl: '4xl',
-    '2xl': '5xl'
+    '2xl': '5xl',
   },
   fontWeight: 'extrabold',
-  mb: 8
+  mb: 8,
 }
 
 const About: FC<Props> = ({ onClick }) => {
@@ -31,22 +31,29 @@ const About: FC<Props> = ({ onClick }) => {
           md: '4rem',
           lg: '5rem',
           xl: '5rem',
-          '2xl': '6rem'
+          '2xl': '6rem',
         }}
         mb={4}
       >
         Hey you! 😀
       </Heading>
       <Text {...textProps}>
-        My name is Aakash Maurya. I am a web developer based in Pune,
-        Maharashtra. Well, I don&apos;t consider myself as just a &quot;web
+        My name is Aakash Maurya. I am a web developer based in{' '}
+        <a
+          href={
+            'https://www.google.co.in/search?q=udaipur&source=lnms&tbm=isch'
+          }
+        >
+          <u>Udaipur</u>
+        </a>
+        , Rajasthan. Well, I don&apos;t consider myself as just a &quot;web
         developer&quot;; I am a problem solver😁.
       </Text>
       <Text {...textProps}>
-        My key strength is debugging the code and problem-solving. 
-        I believe <i>finding the cause is 50% of the solution to the problem</i>. 
-        I have over 3.5 years of experience in developing
-        micro-frontend architecture applications. Majorly working with ReactJS.
+        My key strength is debugging the code and problem-solving. I believe{' '}
+        <i>finding the cause is 50% of the solution to the problem</i>. I have
+        over 4 years of experience in developing micro-frontend architecture
+        applications. Majorly working with ReactJS.
       </Text>
       <Text {...textProps} mb={0}>
         Apart from work I like to travel around places and{' '}
@@ -56,7 +63,8 @@ const About: FC<Props> = ({ onClick }) => {
         . I also watch a lot of movies and tv series🎬.
       </Text>
       <Text {...textProps}>
-        I am addicted to music🎧. You will always find me with my earphones on.
+        I love listening to music🎧. Most of the time you will find me with my
+        earphones on.
       </Text>
       <Text {...textProps}>
         I am a very imaginative and open-minded person and I am not afraid to
@@ -74,19 +82,8 @@ const About: FC<Props> = ({ onClick }) => {
         that bad being overly optimistic, right, or is it?🤔
       </Text>
       <Text {...textProps}>
-        At present, I&apos;m working in{' '}
-        <a href={'https://www.dell.com/en-us/blog/tags/dell-emc/'}>
-          <u>Dell EMC</u>
-        </a>{' '}
-        as Software Engineer 2 and exploring the beauty of{' '}
-        <a
-          href={
-            'https://www.google.co.in/search?q=udaipur&source=lnms&tbm=isch'
-          }
-        >
-          <u>Udaipur</u>
-        </a>
-        .
+        At present, I&apos;m working as a freelance web developer and always
+        looking for opportunities to work with great people and organisations.
       </Text>
       <Text {...textProps}>
         Fancy a chat,{' '}
